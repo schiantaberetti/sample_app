fd632701d79630480efd78632b081c9fb77d3b27
Baldo <baldo@lilik.it>
2012-09-20 15:17:25 +0200

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,5 +19,5 @@ class User < ActiveRecord::Base
 	validates :password, :length => {:minimum => 6}, :presence => true
 	validates :password_confirmation, presence: true
 
-	before_save { |user| user.email = email.downcase }
+	before_save { self.email.downcase! }
 end
