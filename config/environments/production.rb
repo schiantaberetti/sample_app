8b38a12b17ad97edbce7b4b263c856c67a1a7cfe
Baldo <baldo@lilik.it>
2012-09-20 16:14:55 +0200

    upgrade gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.gure do
+SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
